7aa0aba8822a25aa3d5ddf7920ab46517d541768
yangisha <yangishaa@gmail.com>
2020-09-19 09:12:46 +0545

    Updates

---
+++ precache-manifest.e322dae15dc5c70ffa63c53360842d19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c0c95568a995474b90b7a05104869ec",
+    "url": "/Basic_Calculator/index.html"
+  },
+  {
+    "revision": "490048ec0967c53a5244",
+    "url": "/Basic_Calculator/static/css/main.a5876680.chunk.css"
+  },
+  {
+    "revision": "ecada51e1a3334c42b99",
+    "url": "/Basic_Calculator/static/js/2.e6f076d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Basic_Calculator/static/js/2.e6f076d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "490048ec0967c53a5244",
+    "url": "/Basic_Calculator/static/js/main.5bced723.chunk.js"
+  },
+  {
+    "revision": "bcce16b9481e8882cfd1",
+    "url": "/Basic_Calculator/static/js/runtime-main.c37ed35f.js"
+  }
+]);
